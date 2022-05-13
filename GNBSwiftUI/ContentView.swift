bd571ebf12182a7cb2e006e4c73e27458820ed78
jaimeaar <jaimeaar@ext.bershka.com>
2022-05-13 14:16:25 +0200

    first commit

---
+++ GNBSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GNBSwiftUI
+//
+//  Created by Jaime Alcántara on 13/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GNBSwiftUI/GNBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNBSwiftUIApp.swift
+//  GNBSwiftUI
+//
+//  Created by Jaime Alcántara on 13/5/22.
+//
+
+import SwiftUI
+
+@main
+struct GNBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
